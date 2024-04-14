7e2405ebfaeb6a5a0ddec46f164728a6e424f01f
jandres-vega <jandresvega@itsa.edu.co>
2024-04-14 15:03:30 -0500

    Add styles globals y data util

---
+++ src/app/utils/dataProjects.ts
@@ -0,0 +1,40 @@
+type Project = {
+  name_project: string;
+  image: string;
+}
+
+const dataProjects:Project[] = [
+  {
+    "name_project": "CEET PARKING",
+    "image": "https://blackstoneam.com/wp-content/uploads/2021/10/parking-Lot-with-cars.jpeg"
+  },
+  {
+    "name_project": "SHOP STORE",
+    "image": "https://www.indiafilings.com/learn/wp-content/uploads/2023/03/How-can-I-register-my-shop-and-establishment-online.jpg"
+  },
+  {
+    "name_project": "VIDEOGAMES PI",
+    "image": "https://media.gq-magazine.co.uk/photos/645b5c3c8223a5c3801b8b26/16:9/w_2560%2Cc_limit/100-best-games-hp-b.jpg"
+  },
+  {
+    "name_project": "TODO APP",
+    "image": "https://media.geeksforgeeks.org/wp-content/uploads/20230606125727/gfg.png"
+  },
+  {
+    "name_project": "CLONE TREVOL",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRgQwQHBBRvYVjSqHcnzRAx_rY1gw6hhvQqAdtDSkH1g&s"
+  },
+  {
+    "name_project": "CLONE SPOTIFY",
+    "image": "https://repository-images.githubusercontent.com/499762703/0a2ddd16-530b-4f63-9cf8-fe40d591c8eb"
+  },
+  {
+    "name_project": "CHAT DEMO TELEGRAM",
+    "image": "https://user-images.githubusercontent.com/5776439/40442974-c107cb4a-5e79-11e8-8af1-4d2c8be14f48.gif"
+  }
+]
+
+export {
+  dataProjects,
+  Project
+}
