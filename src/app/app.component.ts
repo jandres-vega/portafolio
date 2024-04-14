84316292b8251aa094bf66eb4d3f3906ba040907
jandres-vega <jandresvega@itsa.edu.co>
2024-04-14 15:02:54 -0500

    Add components a app main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CardComponent} from "./components/card/card.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {ListProjectComponent} from "./container/list-project/list-project.component";
+import {WelcomeComponent} from "./components/welcome/welcome.component";
+import {FormContactComponent} from "./components/form-contact/form-contact.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CardComponent,
+    HeaderComponent,
+    ListProjectComponent,
+    WelcomeComponent,
+    FormContactComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'portafolio';
-}
+export class AppComponent {}
