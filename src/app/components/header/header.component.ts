04303ff06efbdb40c5b0b9b533fccec8cce9bf7d
jandres-vega <jandresvega@itsa.edu.co>
2024-04-14 22:29:24 -0500

    Add component na-header y aplicate

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {NavHeaderComponent} from "../nav-header/nav-header.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NavHeaderComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
